6b2474c166b63ab318f6bf7a29226532f27232f3
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-12-05 09:35:41 +0000

    completed the internal

---
+++ internal-2/main.py
@@ -0,0 +1,10 @@
+
+from math_operations import math 
+ 
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("a + b = ", math.add(a, b))
+print("a - b = ", math.subtract(a, b))
+print("a * b = ", math.multiply(a, b))
+print("a / b = ", math.divide(a, b))
